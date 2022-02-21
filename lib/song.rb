1ecd6cec67efd99870614e758f11cd78d70edf19
Rose Jean Louis <r.d.jeanlouis01@gmail.com>
2022-02-21 18:44:49 -0500

    done

---
+++ lib/song.rb
@@ -0,0 +1,37 @@
+require "pry"
+
+class Song
+    attr_accessor :name, :artist, :genre
+    
+    def initialize(name, artist, genre)
+        @name = name
+        @artist = artist
+        @genre = genre
+        @@count += 1
+        @@artists << artist
+        @@genres << genre
+    end
+
+    @@count = 0
+    @@artists = []
+    @@genres = []    
+
+    def self.count
+        @@count
+    end
+
+    def self.artists
+        @@artists.uniq
+    end
+
+    def self.genres
+        @@genres.uniq
+    end
+
+    def self.genre_count
+        genre_list = {}
+
+        
+
+    end
+end
