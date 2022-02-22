9a4429a0bb70a5c9432fb267cc7179a9a7749982
Rose Jean Louis <r.d.jeanlouis01@gmail.com>
2022-02-21 20:38:37 -0500

    done

--- lib/song.rb
+++ lib/song.rb
@@ -26,12 +26,31 @@ class Song
 
     def self.genres
         @@genres.uniq
+        # binding.pry
     end
 
     def self.genre_count
         genre_list = {}
+        @@genres.each do |each_genre|
+            if genre_list[each_genre]
+                genre_list[each_genre] += 1
+            else
+                genre_list[each_genre] = 1
+            end
+        end   
+        genre_list
+    end
+    
 
-        
-
+    def self.artist_count
+        artist_list = {}
+        @@artists.each do |each_artist|
+            if artist_list[each_artist]
+                artist_list[each_artist] += 1
+            else
+                artist_list[each_artist] = 1
+            end
+        end
+        artist_list
     end
 end
